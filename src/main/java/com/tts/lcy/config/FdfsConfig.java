34149220fa382aa39d366812598de9967c85d0a4
toby.li <toby.li@easternphoenix.com>
2020-07-29 14:18:15 +0800

    toby

---
+++ src/main/java/com/tts/lcy/config/FdfsConfig.java
@@ -0,0 +1,16 @@
+package com.tts.lcy.config;
+
+import com.github.tobato.fastdfs.FdfsClientConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
+
+/**
+ * Created by Toby.Li on 2020/7/28.
+ */
+@Configuration
+@Import(FdfsClientConfig.class)
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+public class FdfsConfig {
+}
---
+++ src/test/java/com/tts/lcy/FastdfsApplicationTests.java
@@ -0,0 +1,41 @@
+package com.tts.lcy;
+
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.domain.ThumbImageConfig;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = FastdfsApplication.class)
+public class FastdfsApplicationTests {
+
+
+	@Autowired
+	private FastFileStorageClient storageClient;
+	@Autowired
+	private ThumbImageConfig thumbImageConfig;
+
+	@Test
+	public void contextLoads() throws FileNotFoundException {
+		File file = new File("C:/Users/Administrator/Pictures/表情图/1.gif");
+		System.out.println(file.length());
+		StorePath storePath = storageClient.uploadFile(new FileInputStream(file), file.length(), "gif", null);
+		System.out.println(storePath.getFullPath());
+		System.out.println(storePath.getPath());
+
+		// 获取缩略图路径
+		String path = thumbImageConfig.getThumbImagePath(storePath.getPath());
+		System.out.println(path);
+
+
+	}
+
+}
